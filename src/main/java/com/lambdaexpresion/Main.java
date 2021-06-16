d48ee5b40d2d28f49707c59b799ddf1ff239ca65
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-16 12:29:34 +0530

    ADDED:To multipale way method call in lambda expression

--- src/main/java/com/lambdaexpresion/Main.java
+++ src/main/java/com/lambdaexpresion/Main.java
@@ -4,21 +4,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My system starts");
 
-        /*MyInter myInter = new MyInterImp();
+        //regular object throw method call
+        MyInter myInter = new MyInterImp();
         myInter.sayHello();
-    */
 
-       /* MyInter myInter = new MyInter() {
+        //anonymous class
+        MyInter myInter1 = new MyInter() {
             @Override
             public void sayHello() {
                 System.out.println("this second anonymous class");
             }
         };
-        myInter.sayHello();*/
+        myInter.sayHello();
 
         //lambda expression
 
-        MyInter myInter = () -> System.out.println("This is third method lambda expression");
-        myInter.sayHello();
+        MyInter myInter2 = () -> System.out.println("This is third method lambda expression");
+        myInter2.sayHello();
     }
 }
